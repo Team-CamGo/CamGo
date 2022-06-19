ace888dfdb2170d7bc587315267e8351c750c7ef
minhuikim <kmh940@gmail.com>
2022-06-19 21:07:06 +0900

    2022-06-19 김민희 mapper

--- CamGo/src/main/java/com/example/demo/mapper/Mapper.java
+++ CamGo/src/main/java/com/example/demo/mapper/Mapper.java
@@ -1,5 +1,10 @@
 package com.example.demo.mapper;
 
-public interface Mapper {
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+@Mapper
+public interface Mapper {
+	@Select("select sysdate from dual")
+    public String getTime();
 }
