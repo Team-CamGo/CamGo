73c930f9d2e6129f474f2b3c4d78077d862bd7b4
ohguohgu <aran_93@naver.com>
2022-06-19 15:36:54 +0900

    20220619 project push

---
+++ CamGo/src/test/java/com/example/demo/CamGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
