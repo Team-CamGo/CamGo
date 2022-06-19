872b2785edf9dfd553abaf3b87701f5310d738e1
minhuikim <kmh940@gmail.com>
2022-06-19 21:47:11 +0900

    20220619 mapperTest

---
+++ CamGo/src/test/java/com/example/demo/MapperTest.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import com.example.demo.mapper.CamgoMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class MapperTest {
+
+    @Autowired
+    private CamgoMapper timeMapper;
+
+    @Test
+    public void testGetTime() {
+        log.info("timeMapper class name: " + timeMapper.getClass().getName());
+        log.info("timeMapper time: " + timeMapper.getTime());
+    }
+}
