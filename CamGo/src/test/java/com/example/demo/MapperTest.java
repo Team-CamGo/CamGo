cedd7bc936453b3b3883e2ff2ac9f0cbf5e116ed
minhuikim <kmh940@gmail.com>
2022-06-19 22:00:05 +0900

    20220619 DB connect Test

--- CamGo/src/test/java/com/example/demo/MapperTest.java
+++ CamGo/src/test/java/com/example/demo/MapperTest.java
@@ -1,21 +1,24 @@
 package com.example.demo;
 
 import com.example.demo.mapper.CamgoMapper;
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Slf4j
+//@Slf4j
 @SpringBootTest
 public class MapperTest {
 
     @Autowired
-    private CamgoMapper timeMapper;
+    private CamgoMapper camgoMapper;
 
     @Test
     public void testGetTime() {
-        log.info("timeMapper class name: " + timeMapper.getClass().getName());
-        log.info("timeMapper time: " + timeMapper.getTime());
+//        log.info("timeMapper class name: " + timeMapper.getClass().getName());
+//        log.info("timeMapper time: " + timeMapper.getTime());
+        
+        System.out.print("Test class name : " + camgoMapper.getClass().getName());
+        System.out.print("Test DB time: " + camgoMapper.getTime());
     }
 }
