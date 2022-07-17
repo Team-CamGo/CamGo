daf266ee98f619e2ef25c859b17f743209f94a1e
ohguohgu <aran_93@naver.com>
2022-07-17 21:08:20 +0900

    20220717 index commit

--- CamGo/src/main/java/com/example/demo/CamGoApplication.java
+++ CamGo/src/main/java/com/example/demo/CamGoApplication.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CamGoApplication {
 
+	@RequestMapping("/")
+	public String index() {
+		return "hello world";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CamGoApplication.class, args);
 	}
