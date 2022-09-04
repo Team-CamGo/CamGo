e5cb135e45287093b779a2d6e5175fbbbf1949a7
minhuikim <kmh940@gmail.com>
2022-09-04 20:30:20 +0900

    20220904 compingDetails

--- CamGo/src/main/java/com/example/demo/controller/campingController.java
+++ CamGo/src/main/java/com/example/demo/controller/campingController.java
@@ -1,19 +1,25 @@
 package com.example.demo.controller;
 
-import org.springframework.context.annotation.Conditional;
+// import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class campingController {
 
-  @RequestMapping("/campinglist")
-  public String campingList() {
-    return "/camping/campingList.html";
-  }
+    @RequestMapping("/campinglist")
+    public String campingList() {
+        return "/camping/campingList.html";
+    }
 
-  @RequestMapping("/campingdetail")
-  public String campingDetail() {
-    return "/camping/campingDetail.html";
-  }
+    @RequestMapping("/campingdetail")
+    public String campingDetail() {
+        return "/camping/campingDetail.html";
+    }
+
+    @RequestMapping("/campdetail")
+    public String campDetail() {
+        return "/camping/campDetail.html";
+    }
+
 }
