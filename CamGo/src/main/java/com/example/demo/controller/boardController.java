00087e204f242a96443df279b7a35d90e698ec26
minhuikim <kmh940@gmail.com>
2022-09-04 20:46:43 +0900

    20220904 controller 주석 추가

--- CamGo/src/main/java/com/example/demo/controller/boardController.java
+++ CamGo/src/main/java/com/example/demo/controller/boardController.java
@@ -7,21 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class boardController {
 
+  /*
+   * 공지사항 리스트
+   */
   @RequestMapping("/notice")
   public String notice() {
     return "/board/notice";
   }
 
+  /*
+   * 공지사항 수정
+   */
   @RequestMapping("/modify")
   public String modify() {
     return "/board/modify";
   }
 
+  /*
+   * 공지사항 작성
+   */
   @RequestMapping("/write")
   public String write() {
     return "/board/write";
   }
 
+  /*
+   * 공지사항 문의
+   */
   @RequestMapping("/answer")
   public String answer() {
     return "/board/answer";
--- CamGo/src/main/java/com/example/demo/controller/campingController.java
+++ CamGo/src/main/java/com/example/demo/controller/campingController.java
@@ -7,19 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class campingController {
 
+    /*
+     * 캠핑지 목록
+     */
     @RequestMapping("/campinglist")
     public String campingList() {
         return "/camping/campingList.html";
     }
 
+    /*
+     * 캠핑지 상세페이지
+     */
     @RequestMapping("/campingdetail")
     public String campingDetail() {
         return "/camping/campingDetail.html";
-    }
-
-    @RequestMapping("/campdetail")
-    public String campDetail() {
-        return "/camping/campDetail.html";
     }
 
 }
