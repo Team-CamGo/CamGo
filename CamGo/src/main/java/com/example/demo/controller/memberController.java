82426df3638d94f7b52471014cc6b27156cb2f8c
ssd256 <zmffn2@gmail.com>
2022-09-16 00:11:48 +0900

    memberController 주석 수정

--- CamGo/src/main/java/com/example/demo/controller/memberController.java
+++ CamGo/src/main/java/com/example/demo/controller/memberController.java
@@ -7,73 +7,97 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class memberController {
 
-  /* 회원탈퇴 */
+  /*
+   * 회원탈퇴
+   */
   @RequestMapping("/withdrawal")
   public String withdrawal() {
     return "/member/withdrawal";
   }
 
-  /* 회원가입 - 정보 동의 */
+  /*
+   * 회원가입 - 정보 동의
+   */
   @RequestMapping("/join")
   public String join() {
     return "/member/join";
   }
 
-  /* 회원가입 - 정보 입력 */
+  /*
+   * 회원가입 - 정보 입력
+   */
   @RequestMapping("/joininfo")
   public String join_info() {
     return "/member/joinInfo";
   }
 
-  /* 마이페이지 - 회원 */
+  /*
+   * 마이페이지 - 회원
+   */
   @RequestMapping("/myInfo")
   public String myInfo() {
     return "/member/myInfo";
   }
 
-  /* 마이페이지 - 회원 - 예약내역조회 */
+  /*
+   * 마이페이지 - 회원 - 예약내역조회
+   */
   @RequestMapping("/reservation")
   public String reservation() {
     return "/member/reservation";
   }
 
-  /* 마이페이지 - 회원 - 결제내역조회 */
+  /*
+   * 마이페이지 - 회원 - 결제내역조회
+   */
   @RequestMapping("/buyProduct")
   public String buy_product() {
     return "/member/buyProduct";
   }
 
-  /* 마이페이지 - 회원 - 좋아요 */
+  /*
+   * 마이페이지 - 회원 - 좋아요
+   */
   @RequestMapping("/like")
   public String like_camping() {
     return "/member/likeCamping";
   }
 
-  /* 로그인 */
+  /*
+   * 로그인
+   */
   @RequestMapping("/login")
   public String login() {
     return "/member/login";
   }
 
-  /* 아이디 찾기 */
+  /*
+   * 아이디 찾기
+   */
   @RequestMapping("/findId")
   public String findId() {
     return "/member/findId";
   }
 
-  /* 비밀번호 찾기 */
+  /*
+   * 비밀번호 찾기
+   */
   @RequestMapping("/findPw")
   public String findPw() {
     return "/member/findPw";
   }
 
-  /* 리뷰  작성 페이지 */
+  /*
+   * 리뷰  작성 페이지
+   */
   @RequestMapping("/review")
   public String review() {
     return "/member/review";
   }
 
-  /* 문의글 작성 페이지 */
+  /*
+   * 문의글 작성 페이지
+   */
   @RequestMapping("/inquiry")
   public String inquiry() {
     return "/member/inquiry";
