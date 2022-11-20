6fc23dfb4223f80f6ce9f31a39660a65eb253d1b
ssd256 <zmffn2@gmail.com>
2022-11-20 20:16:39 +0900

    수정

--- CamGo/src/main/java/com/example/demo/controller/memberController.java
+++ CamGo/src/main/java/com/example/demo/controller/memberController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 // import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class memberController {
